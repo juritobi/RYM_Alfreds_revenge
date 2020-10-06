91f80f92be38869aff2e7ee69f5da9f9c3ff3128
juritobi <juritobi@gmail.com>
2020-10-06 17:28:45 +0200

    woops

--- crunch/src/sys/input.c
+++ crunch/src/sys/input.c
@@ -1,13 +1,7 @@
 #include "input.h"
 #include <man/man_ent.h>
 
-<<<<<<< HEAD
 const i8 jumptable[] = {-2, -2, -2, -2, 0, 0};
-=======
-const i8 jumptable[] = {-2, -2, -2, -2, -2, 0,0};
-i8 contador_de_mierda = 5;
-
->>>>>>> 315a3e8b7ddc6e25a95427afff5c992434d57315
 
 void sys_input_one(ent_t* ent){
     if (ent->type & e_t_input)
