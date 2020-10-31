825bd5fe86bc72cccfeff787f227316420b32da3
juritobi <juritobi@gmail.com>
2020-10-31 09:42:17 +0100

    clipping

--- crunch/src/sys/render.c
+++ crunch/src/sys/render.c
@@ -1,9 +1,11 @@
 #include "render.h"
 #include <constants.h>
 
-u8 next_free_index;
+tile* tiles_pointer;
 tile tiles[60];
-u16 null_end_tiles;
+
+ent_t** ren_ents_pointer;
+ent_t* ren_ents[12];
 
 
 void sys_ren_draw_tilemap(u8* tilemap){
@@ -11,12 +13,14 @@ void sys_ren_draw_tilemap(u8* tilemap){
     cpct_etm_drawTilemap4x8_ag(CPCT_VMEM_START+240, tilemap);
 }
 void sys_ren_set_tile(u8 tile_index, u8 x, u8 y){
-    tile to_ren;
-    to_ren.sprite = tileset + tile_index*tileset_length;
-    to_ren.pos = cpct_getScreenPtr(CPCT_VMEM_START, x, y);
-    tiles[next_free_index].sprite = to_ren.sprite;
-    tiles[next_free_index].pos = to_ren.pos;
-    next_free_index++;
+    if(x>=0 && x<=76 && y>=24 && y<=192){
+        tile to_ren;
+        to_ren.sprite = tileset + tile_index*tileset_length;
+        to_ren.pos = cpct_getScreenPtr(CPCT_VMEM_START, x, y);
+        tiles_pointer->sprite = to_ren.sprite;
+        tiles_pointer->pos = to_ren.pos;
+        ++tiles_pointer;
+    }
 }
 void sys_ren_tile(u8 tile_index, u8 x, u8 y){
     tile to_ren;
@@ -26,30 +30,39 @@ void sys_ren_tile(u8 tile_index, u8 x, u8 y){
 }
 
 void sys_ren_init(){
-    cpct_memset(tiles, 0, sizeof(tiles));
-    null_end_tiles = 0;
-    next_free_index= 0;
+    tiles_pointer = tiles;
+    ren_ents_pointer = ren_ents;
 }
-
-
-
 
 void sys_ren_one(ent_t* e){
     cpct_drawSprite (e->sprite, e->memory_pos, e->w, e->h);
 }
+
 void sys_ren_setup_one(ent_t* e){
-    e->memory_pos = cpct_getScreenPtr(CPCT_VMEM_START, e->x, e->y);
+    if(e->x>=0 && e->x+e->w<=80 && e->y>=24 && e->y+e->h<=200){
+        e->memory_pos = cpct_getScreenPtr(CPCT_VMEM_START, e->x, e->y);
+        *ren_ents_pointer = e;
+        ++ren_ents_pointer;
+    }
 }
 void sys_ren_setup(){
     man_ent_forall_type(sys_ren_setup_one, e_t_render);
 }
+
+
 void sys_ren_render(){
-    u8 index = 0;
-    while(tiles[index]){
-        cpct_drawTileZigZagGrayCode4x8_af (tiles[index].pos, tiles[index].sprite);
-        ++index;
+    tiles_pointer->pos = 0;
+    tiles_pointer = tiles;
+    *ren_ents_pointer = 0;
+    ren_ents_pointer = ren_ents;
+    while(tiles_pointer->pos){
+        cpct_drawTileZigZagGrayCode4x8_af (tiles_pointer->pos, tiles_pointer->sprite);
+        ++tiles_pointer;
     }
-    man_ent_forall_type(sys_ren_one, e_t_render);
-    cpct_memset(tiles, 0, sizeof(tiles));
-    next_free_index= 0;
+    while(*ren_ents_pointer){
+        sys_ren_one(*ren_ents_pointer);
+        ++ren_ents_pointer;
+    }
+    ren_ents_pointer = ren_ents;
+    tiles_pointer = tiles;
 }
