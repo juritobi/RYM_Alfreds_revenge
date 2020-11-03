91b92a220ca2de88a703850134ab1f703a2d2eb3
Lotus1311 <josenp1311@gmail.com>
2020-11-03 12:07:51 +0100

    cambios a los mapas BB

--- crunch/src/man/level.c
+++ crunch/src/man/level.c
@@ -128,8 +128,7 @@ const lvl_t i_bot_left_1 = {
         {0, 0, 0},
         {0, 0, 0}
     }
-};
-const lvl_t i_bot_left_2 = {
+};const lvl_t i_bot_left_2 = {
     3,                          //id
     bot_left_2_pack_end,        //this
     &i_bot_left_1,              //top
@@ -140,7 +139,7 @@ const lvl_t i_bot_left_2 = {
     0,
     {
         {e_c_zombi, 52, 88},
-        {e_c_shoot, 72, 64},
+        {e_c_shoot, 68, 64},
         {e_c_zombi, 12, 160},
         {0, 0, 0},
         {0, 0, 0}
@@ -175,7 +174,7 @@ const lvl_t i_bot_left_extra = {
     {
         {e_c_zombi, 52, 176},
         {e_c_zombi, 60, 56},
-        {e_c_shoot, 36, 176},
+        {e_c_shoot, 32, 176},
         {e_c_ghost, 8, 24},
         {0, 0, 0}
     }
@@ -224,7 +223,7 @@ const lvl_t i_bot_left_6 = {
     bl_room,
     0,
     {
-        {e_c_shoot, 76, 64},
+        {e_c_shoot, 72, 64},
         {e_c_sonic, 24, 96},
         {e_c_sonic, 24, 176},
         {e_c_sonic, 56, 176},
@@ -276,7 +275,7 @@ const lvl_t i_bot_right_3 = {
     normal_room,
     0,
     {
-        {e_c_shoot, 76,  80},
+        {e_c_shoot, 72,  80},
         {e_c_sonic, 36, 176},
         {e_c_sonic, 60, 176},
         {0, 0, 0},
@@ -345,8 +344,8 @@ const lvl_t i_bot_right_6 = {
     0,
     {
         {e_c_sonic, 16, 96},
-        {e_c_shoot, 76, 64},
-        {e_c_shoot, 76, 160},
+        {e_c_shoot, 72, 64},
+        {e_c_shoot, 72, 160},
         {0, 0, 0},
         {0, 0, 0}
     }
@@ -482,9 +481,9 @@ const lvl_t i_top_left_7 = {
     0,
     {
         {e_c_sonic, 16, 64},
-        {e_c_shoot, 76, 64},
+        {e_c_shoot, 72, 64},
         {e_c_sonic, 72, 96},
-        {e_c_shoot, 76, 128},
+        {e_c_shoot, 72, 128},
         {0, 0, 0}
     }
 };
@@ -517,8 +516,8 @@ const lvl_t i_top_right_2 = {
     0,
     {
         {e_c_sonic, 40, 176},
-        {e_c_shoot, 76, 104},
-        {e_c_shoot, 76, 136},
+        {e_c_shoot, 72, 104},
+        {e_c_shoot, 72, 136},
         {0, 0, 0},
         {0, 0, 0}
     }
@@ -584,8 +583,8 @@ const lvl_t i_top_right_5 = {
     tr_room,
     0,
     {
-        {e_c_shoot, 76, 112},
-        {e_c_shoot, 76, 64},
+        {e_c_shoot, 72, 112},
+        {e_c_shoot, 72, 64},
         {e_c_sonic, 56, 64},
         {e_c_sonic, 56, 112},
         {0, 0, 0}
@@ -715,10 +714,10 @@ void bl_room(){
     cleared_rooms[0] |= 8; 
 }
 void br_room(){
-    redraw_tile(9,  3, 6);
-    redraw_tile(9,  0, 18);
-    redraw_tile(10,  0, 18);
-    redraw_tile(10,  3, 6);
+    redraw_tile(12,  3, 6);
+    redraw_tile(12,  0, 18);
+    redraw_tile(11,  0, 18);
+    redraw_tile(11,  3, 6);
     redraw_tile(6,  3, 12);
     redraw_tile(16,  3, 12);
     
@@ -726,10 +725,10 @@ void br_room(){
 }
 void tl_room(){
     
-    redraw_tile(11,  3, 6);
-    redraw_tile(11,  0, 18);
-    redraw_tile(12,  0, 18);
-    redraw_tile(12,  3, 6);
+    redraw_tile(9,  3, 6);
+    redraw_tile(9,  0, 18);
+    redraw_tile(10,  0, 18);
+    redraw_tile(10,  3, 6);
     redraw_tile(5,  2, 12);
     redraw_tile(14,  2, 12);
     cleared_rooms[0] |= 1; 
